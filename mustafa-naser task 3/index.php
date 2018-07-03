bcad6f355791dc734f7f644a1cf1543b29052d7d
mustafa <sof9816@gmail.com>
2018-07-03 18:33:58 +0300

    Done task3 and edited task 2

---
+++ mustafa-naser task 3/index.php
@@ -0,0 +1,36 @@
+<?php
+    $arr = array("mustafa","naser","ahmed","mohemmed"); // creating array
+
+    $arr[] = "omar"; //adding element to the array
+    $arr[] = "omar"; //adding element to the array
+
+    echo "<pre>";
+    print_r($arr); //printing array
+    echo "</pre>";
+
+    sort($arr, SORT_REGULAR); //sorting array
+
+    echo "<pre>";
+    print_r($arr); //printing array
+    echo "</pre>";
+
+
+    $rand = array_rand($arr,2); // pick a random element from the array
+
+    echo "-------------------- <br>";
+    echo "<pre>";
+    print_r($rand); //printing random element index 
+    echo "</pre>";
+
+    echo $arr[$rand[0]] . "<br>"; //printing random element
+    echo $arr[$rand[1]]. "<br>";  //printing random element
+    echo "-------------------- <br>";
+
+    $aru = array_unique($arr); // an array that has no diplicates 
+
+    echo "<pre>";
+    print_r($aru); //printing array
+    echo "</pre>";
+
+
+?>
