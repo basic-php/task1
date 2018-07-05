642cfa8510c78386856bd2ddf0d4f2cc4cfb2339
rami <rami@salm.com>
2018-07-04 19:52:42 -0700

    fix some mistake in task2

--- task2/index2.php
+++ task2/index2.php
@@ -1,22 +1,24 @@
 <?php
 // define the function
 function Year($date1,$date2)
+{
+    $msg=" ";
+    //check the date
+    if ($date1<2010 && $date2>2020)
     {
-        //check the date 
-        if ($date1<2010 && $date2>2020)
-            {
-                echo "The date is good";
-            }
-            //print all date
-            else
-            {
-                for ($i=$date1;$i<=$date2;$i++)
-                {
-                    echo $i."<br>";
-                }
-            }
+        return "The date is less than 2010 and greatr than 2020";
     }
-    //call the function
-    Year(2002,2022);
+    //print all date
+    else
+    {
+        for ($i=$date1;$i<=$date2;$i++)
+        {
+            $msg.= $i." ";
+        }
+     return $msg;
+    }
+}
+//call the function
+echo Year(2009,2022);
 
 ?>
