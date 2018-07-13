26fc2ef4b8ed8fbbefa84730e998b9758ed22ecb
TaymoorAbdeen <TaymoorAbdeen@gmail.com>
2018-07-13 11:36:42 +0300

    Task one updated

--- TaymoorAbdeen-Task-1/index.php
+++ TaymoorAbdeen-Task-1/index.php
@@ -3,45 +3,27 @@ $number1 = 10;
 $number2 = 15;
 $number3 = 13;
 
-$mul = $number1  * $number2 * $number3;
+// اضرب رقمين واضع النتيجة في متغير
+$mul = $number1  * $number2;
 
-$mod = $mul % $number1;
-if ($mod == 8)
-echo "This is greater equal to 8".$mod;
-else if ($mod >8)
-echo "This is greater gratet than 8";
-else
-echo "This is another";
 
-$mod = $mul % $number2;
-if ($mod == 8)
-echo "This is greater equal to 8".$mod;
-else if ($mod >8)
-echo "This is greater gratet than 8";
-else
-echo "This is another";
-
+// المتغير باقي القسمة مع الرقم الثالث واضع النتيجة في متغير 
 $mod = $mul % $number3;
+
+// اذا مود يساوي ثمانية 
 if ($mod == 8)
-echo "This is greater equal to 8".$mod;
+
+    // اطبع هذا الجملة مع قيمة مود
+
+    echo "This is greater equal to 8".$mod;
+// والا اذا مود اكبر من ثمانية
 else if ($mod >8)
-echo "This is greater gratet than 8";
+
+    // اطبع هذا الجملة مع قيمة مود مع رقم ثمانية
+    echo "This is greater gratet than 8";
+
+// والا
 else
-echo "This is another";
-
-
-
-/*
-اولا ناخذ مضروب العدد ونضعه في دائم اسمه
-mulثم ناخذ كل عدد ونستخرج باقي الفسمة ونضع النتيجة
-mod
-ثم نقارن
-هل تساوي رقم 8 مع باقي القسمة الذي مخزون في
-mod
-اذا نعم: اطبع نص
-و اذا كان اكبر من 8
-اذا نعم: اطبع نص مع رقم دون
-والا
-اطبع نص
-*/
+    // اطبع هذا الجملة  
+    echo "This is another";
 ?>
