d2292b874d132bc056ad4bdc81570777aba5f7a1
AreyXo <ari.muayad@gmail.com>
2018-07-21 18:34:12 +0300

    Completed

--- task4/index.php
+++ task4/index.php
@@ -1,11 +1,36 @@
 <?php
-$st = "this is a beautiful day";
+// first requirement
+$st = "this is@ a beautiful@ day";
 $arr = explode(" ",$st);
-
 print_r($arr);
+print "<br>";
+ 
 
- print_r($arr);
+// second
+
+$key = array_rand($arr,1);
+ print_r($arr[$key]);
+ print "<br>";
 
 
+ //third
+echo ucfirst(array_shift($arr)).'<br>';
+
+// fourth
+foreach($arr as $word)
+{
+ if(strlen($word)>6)
+ echo "YES <br>";
+
+ else echo "NO <br>";
+}
+
+//fifth
+$tagLess = array();
+foreach($arr as $word)
+{ 
+    $tagLess[] = str_replace("@","/",$word);
+}
+print_r($tagLess);
 
 ?>
